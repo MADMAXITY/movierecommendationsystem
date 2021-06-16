e69ab366ba2021b16331756a21a0b96a22707d8b
Shankar Jadhav <51054988+MADMAXITY@users.noreply.github.com>
2021-06-16 18:01:59 +0530

    Delete movies directory

--- movies/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from movies.models import Movies
-
-# Register your models here.
-class MoviesAdmin(admin.ModelAdmin):
-    pass
-
-
-admin.site.register(Movies, MoviesAdmin)
